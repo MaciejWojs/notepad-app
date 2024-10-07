22caa54391d2126195a52118eb8081c956eeccee
MaciejWojs <misiek111403@gmail.com>
2024-10-07 20:58:27 +0200

    testing repository changes co-working

--- app/src/main/java/pl/maciejwojs/ar00k/bestnotepadevercreaated/MainActivity.kt
+++ app/src/main/java/pl/maciejwojs/ar00k/bestnotepadevercreaated/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import pl.maciejwojs.ar00k.bestnotepadevercreaated.ui.theme.BestNotepadEverCreatedTheme
 
 class MainActivity : ComponentActivity() {
+    //Komentarz w kodzie
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
