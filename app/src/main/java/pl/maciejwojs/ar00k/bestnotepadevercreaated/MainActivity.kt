383e159c6f69a93e7d6667d9eeb036fd6684099c
MaciejWojs <misiek111403@gmail.com>
2024-10-12 21:12:58 +0200

    Konfiguracja doxygen, aktualizacja .gitignore, komentarze do doxygen

--- app/src/main/java/pl/maciejwojs/ar00k/bestnotepadevercreaated/MainActivity.kt
+++ app/src/main/java/pl/maciejwojs/ar00k/bestnotepadevercreaated/MainActivity.kt
@@ -15,7 +15,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import pl.maciejwojs.ar00k.bestnotepadevercreaated.ui.theme.BestNotepadEverCreatedTheme
 
+/**
+ * Główna aktywność aplikacji BestNotepadEverCreated.
+ *
+ * Ta aktywność rozszerza [ComponentActivity] i służy jako główny punkt wejścia do aplikacji.
+ * Używa Jetpack Compose do wyświetlania interfejsu użytkownika i wyświetla wiadomości Toast w trakcie zmian cyklu życia.
+ */
 class MainActivity : ComponentActivity() {
+
+    /**
+     * Wywoływana, gdy aktywność jest po raz pierwszy tworzona.
+     *
+     * Ta metoda ustawia interfejs użytkownika dla aktywności przy użyciu Jetpack Compose.
+     *
+     * @param savedInstanceState Pakiet zawierający poprzednio zapisany stan aktywności.
+     * @see ComponentActivity.onCreate
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -31,28 +46,63 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    /**
+     * Wywoływana, gdy aktywność staje się widoczna dla użytkownika.
+     *
+     * Wyświetla wiadomość Toast, aby wskazać, że aktywność jest w stanie onStart().
+     *
+     * @see ComponentActivity.onStart
+     */
     override fun onStart() {
         super.onStart()
         Toast.makeText(applicationContext, "onStart()", Toast.LENGTH_LONG).show()
     }
 
+    /**
+     * Wywoływana przed zniszczeniem aktywności.
+     *
+     * Wyświetla wiadomość Toast, aby wskazać, że aktywność jest w stanie onDestroy().
+     *
+     * @see ComponentActivity.onDestroy
+     */
     override fun onDestroy() {
         super.onDestroy()
         Toast.makeText(applicationContext, "onDestroy()", Toast.LENGTH_LONG).show()
     }
 
+    /**
+     * Wywoływana, gdy system zamierza wznowić wcześniejszą aktywność.
+     *
+     * Wyświetla wiadomość Toast, aby wskazać, że aktywność jest w stanie onPause().
+     *
+     * @see ComponentActivity.onPause
+     */
     override fun onPause() {
         super.onPause()
         Toast.makeText(applicationContext, "onPause()", Toast.LENGTH_LONG).show()
     }
 
+    /**
+     * Wywoływana po zatrzymaniu aktywności i ponownym uruchomieniu.
+     *
+     * Wyświetla wiadomość Toast, aby wskazać, że aktywność jest w stanie onRestart().
+     *
+     * @see ComponentActivity.onRestart
+     */
     override fun onRestart() {
         super.onRestart()
         Toast.makeText(applicationContext, "onRestart()", Toast.LENGTH_LONG).show()
     }
-
 }
 
+/**
+ * Funkcja kompozytowa, która wyświetla wiadomość powitalną.
+ *
+ * Ta funkcja wyświetla tekst powitalny z użyciem podanej nazwy.
+ *
+ * @param name Nazwa, która ma być wyświetlana w wiadomości powitalnej.
+ * @param modifier [Modifier] stosowany do układu kompozytowego. Domyślnie jest to pusty modifier.
+ */
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -61,6 +111,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+/**
+ * Funkcja podglądu dla kompozytu [Greeting].
+ *
+ * Wyświetla podgląd wiadomości powitalnej z "Android" jako nazwą.
+ */
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
