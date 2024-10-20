9d9de9633ed185a4b1133eedb9654c635d371d75
MaciejWojs <misiek111403@gmail.com>
2024-10-20 14:58:49 +0200

    Layout -> wersja pre alpha

--- app/src/main/java/pl/maciejwojs/ar00k/bestnotepadevercreaated/MainActivity.kt
+++ app/src/main/java/pl/maciejwojs/ar00k/bestnotepadevercreaated/MainActivity.kt
@@ -1,19 +1,27 @@
 package pl.maciejwojs.ar00k.bestnotepadevercreaated
 
-import android.health.connect.datatypes.units.Length
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import pl.maciejwojs.ar00k.bestnotepadevercreaated.ui.theme.BestNotepadEverCreatedTheme
+import java.util.Random
 
 /**
  * Główna aktywność aplikacji BestNotepadEverCreated.
@@ -36,11 +44,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BestNotepadEverCreatedTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+                Column(Modifier.verticalScroll(rememberScrollState(1))) {
+//                    var i = 0
+                    for (i in 0..7) {
+                        val rnd: Random = Random()
+                        val color: Color =
+                            Color(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+                        Box(
+                            Modifier
+                                .background(color)
+                                .padding(5.dp)
+                                .scale(0.9f)
+                                .fillMaxWidth()
+                                .height(250.dp)
+                        )
+                        {
+                            Greeting(name = "Witaj swiecie")
+                        }
+                    }
                 }
             }
         }
