29ce1478866d3081a4be5f7976f289c7e1be24df
MaciejWojs <misiek111403@gmail.com>
2024-10-26 17:44:14 +0200

    Dodanie biblioteki do stron

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp") version "2.0.0-1.0.24"
 }
 
 android {
@@ -56,4 +57,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("io.github.raamcosta.compose-destinations:core:1.11.7")
+    ksp("io.github.raamcosta.compose-destinations:ksp:1.11.7")
 }
