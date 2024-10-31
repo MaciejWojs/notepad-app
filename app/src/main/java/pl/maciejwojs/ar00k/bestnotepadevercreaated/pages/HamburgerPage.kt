618b14f0848ae3202442bda8067d4e71ea99f1ba
MaciejWojs <misiek111403@gmail.com>
2024-11-01 00:04:03 +0100

    Wieloplikowość, MainActivity.kt

---
+++ app/src/main/java/pl/maciejwojs/ar00k/bestnotepadevercreaated/pages/HamburgerPage.kt
@@ -0,0 +1,123 @@
+package pl.maciejwojs.ar00k.bestnotepadevercreaated.pages
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import pl.maciejwojs.ar00k.bestnotepadevercreaated.content.GenerateIconButton
+import pl.maciejwojs.ar00k.bestnotepadevercreaated.ui.theme.BestNotepadEverCreatedTheme
+
+@Composable
+fun HamburgerPage(navigator: NavController) {
+    BestNotepadEverCreatedTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+            Column(modifier = Modifier.padding(innerPadding)) {
+                Row(
+                    Modifier
+                        .height(50.dp)
+                        .fillMaxWidth()
+                        .background(MaterialTheme.colorScheme.onSecondary)
+                        .padding(horizontal = 8.dp), //1 Add some padding to the row
+                    horizontalArrangement = Arrangement.SpaceBetween // Arrange items in row
+                ) {
+//                            Row(
+//                                Modifier
+//                                    .fillMaxWidth()
+//                            ) {
+                    GenerateIconButton(
+                        icon = Icons.AutoMirrored.Filled.ArrowBack, "Back to main screen"
+                    ) {
+                        if (!navigator.popBackStack()) {
+                            // Przejdź do głównego ekranu lub innego widoku początkowego
+                            navigator.navigate("MainPage")
+                        }
+
+                    }
+                    var searchCounter: Int = 0
+                    Row(
+                        Modifier
+                            .clip(RoundedCornerShape(25.dp))
+                            .align(Alignment.CenterVertically)
+                            .fillMaxHeight(0.55f)
+                            .fillMaxWidth(0.75f)
+//                                    .background(Color.Red)
+                            .border(
+                                width = 2.dp,
+                                color = MaterialTheme.colorScheme.outline,
+                                shape = RoundedCornerShape(25.dp)
+                            )
+                            .clickable {
+//                                Toast
+//                                    .makeText(
+//                                        applicationContext,
+//                                        "Clicked ${++searchCounter} ${if (searchCounter < 2) "time" else "times"}",
+//                                        Toast.LENGTH_LONG
+//                                    )
+//                                    .show()
+                            },
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+
+                        Text(
+                            text = "Search", Modifier.padding(start = 5.dp)
+//                                            .align(alignment = Alignment.CenterVertically)
+                        )
+
+                        GenerateIconButton(Icons.Default.Search, "Search menu") {}
+                    }
+
+                    GenerateIconButton(
+                        icon = Icons.Filled.Settings,
+                        "Settings",
+                        transparent = true,
+                        isEnabled = false,
+                    ) {}
+                }
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .verticalScroll(
+                            rememberScrollState(1)
+
+                        )
+
+                ) {
+                    for (i in 0..3) {
+                        Spacer(modifier = Modifier.height(50.dp))
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/pl/maciejwojs/ar00k/bestnotepadevercreaated/pages/MainPage.kt
@@ -0,0 +1,160 @@
+package pl.maciejwojs.ar00k.bestnotepadevercreaated.pages
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import pl.maciejwojs.ar00k.bestnotepadevercreaated.content.GenerateIconButton
+import pl.maciejwojs.ar00k.bestnotepadevercreaated.content.GenerateNote
+import pl.maciejwojs.ar00k.bestnotepadevercreaated.settings.roundness
+import pl.maciejwojs.ar00k.bestnotepadevercreaated.ui.theme.BestNotepadEverCreatedTheme
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+
+@Composable
+fun MainPage(navigator: NavController) {
+    BestNotepadEverCreatedTheme {
+        Scaffold(modifier = Modifier.fillMaxSize(), floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    navigator.navigate("CreateNotePage")
+                },
+                containerColor = MaterialTheme.colorScheme.onSecondary,
+                shape = CircleShape,
+                elevation = FloatingActionButtonDefaults.elevation(20.dp)
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
+        }) { innerPadding ->
+            Column(modifier = Modifier.padding(innerPadding)) {
+                Row(
+                    Modifier
+                        .height(50.dp)
+                        .fillMaxWidth()
+                        .background(MaterialTheme.colorScheme.onSecondary)
+                        .padding(horizontal = 8.dp), //1 Add some padding to the row
+                    horizontalArrangement = Arrangement.SpaceBetween // Arrange items in row
+                ) {
+//                            Row(
+//                                Modifier
+//                                    .fillMaxWidth()
+//                            ) {
+                    GenerateIconButton { navigator.navigate("HamburgerPage") }
+                    var searchCounter: Int = 0
+                    Row(
+                        Modifier
+                            .clip(RoundedCornerShape(25.dp))
+                            .align(Alignment.CenterVertically)
+                            .fillMaxHeight(0.55f)
+                            .fillMaxWidth(0.75f)
+//                                    .background(Color.Red)
+                            .border(
+                                width = 2.dp,
+                                color = MaterialTheme.colorScheme.outline,
+                                shape = RoundedCornerShape(25.dp)
+                            )
+                            .clickable {
+//                                Toast
+//                                    .makeText(
+//                                        applicationContext,
+//                                        "Clicked ${++searchCounter} ${if (searchCounter < 2) "time" else "times"}",
+//                                        Toast.LENGTH_LONG
+//                                    )
+//                                    .show()
+                            },
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+
+                        Text(
+                            text = "Search", Modifier.padding(start = 5.dp)
+//                                            .align(alignment = Alignment.CenterVertically)
+                        )
+
+                        GenerateIconButton(Icons.Default.Search, "Search menu") {}
+                    }
+                    GenerateIconButton(icon = Icons.Default.Settings, "Settings") {
+
+                        navigator.navigate("SettingsPage")
+
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                val currentDate = LocalDate.now()
+                val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+                val formattedDate = currentDate.format(formatter)
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .verticalScroll(
+                            rememberScrollState(1)
+
+                        )
+
+                ) {
+                    for (i in 0..20) {
+//                        var color: Color
+//                        if (i % 2 == 0) {
+//                            color = MaterialTheme.colorScheme.primaryContainer
+//                        } else {
+//                            color = MaterialTheme.colorScheme.secondaryContainer
+//                        }
+                        // zaokraglenie musi być zsynchronizowane
+                        GenerateNote(
+                            Modifier
+                                .clip(RoundedCornerShape(roundness.dp))
+//                                        .background(color)
+                                .border(
+                                    width = 2.dp,
+                                    color = MaterialTheme.colorScheme.outline,
+                                    shape = RoundedCornerShape(roundness.dp)
+                                )
+
+                                .padding(7.dp)
+                                .scale(0.9f)
+                                .height(250.dp)
+                                .fillMaxWidth(0.8f),
+                            weight = null,
+                            creationDate = formattedDate
+                        )
+
+                        Spacer(modifier = Modifier.height(50.dp))
+                    }
+                }
+            }
+        }
+    }
+}
