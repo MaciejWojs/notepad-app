b5509c6798880f08f8dc0c830c1be41eca7b8622
ar00k <arkadiusz.ryczek@onet.pl>
2024-11-24 22:41:52 +0100

    addded setting table to database

--- app/src/main/java/pl/maciejwojs/ar00k/bestnotepadevercreaated/NotesDatabase.kt
+++ app/src/main/java/pl/maciejwojs/ar00k/bestnotepadevercreaated/NotesDatabase.kt
@@ -12,12 +12,13 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import pl.maciejwojs.ar00k.bestnotepadevercreaated.db.Note
+import pl.maciejwojs.ar00k.bestnotepadevercreaated.db.Settings
 import pl.maciejwojs.ar00k.bestnotepadevercreaated.db.Tag
 import pl.maciejwojs.ar00k.bestnotepadevercreaated.db.relations.AudioFilesNotesCrossRef
 import pl.maciejwojs.ar00k.bestnotepadevercreaated.db.relations.NotesTagsCrossRef
 
 @Database(
-    entities = [Note::class, Tag::class, NotesTagsCrossRef::class, AudioFilesNotesCrossRef::class],
+    entities = [Note::class, Tag::class, NotesTagsCrossRef::class, AudioFilesNotesCrossRef::class, Settings::class],
     version = 1,
 )
 abstract class NotesDatabase : RoomDatabase() {
